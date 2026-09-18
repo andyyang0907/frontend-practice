05dac49dab856f4db69e01ce7a70f2ceb978e363
Andy <1828376479@qq.com>
2026-09-18 20:16:07 +0800

    添加成绩数据定义

---
+++ score-stat/script.js
@@ -0,0 +1,11 @@
+//script.js
+const score = [
+    {name:'李四',score:92},
+    {name:'王五',score:85},
+    {name:'赵六',score:78},
+    {name:'孙七',score:59},
+    {name:'周八',score:88},
+    {name:'吴九',score:105},
+    {name:'郑十',score:-3},
+];
+console.table(score);
