d06073daf63ce43f67dc6c6dc95e848200d8e4da
Andy <1828376479@qq.com>
2026-09-19 10:38:11 +0800

    输入原始课程成绩

---
+++ GPA-calculation/script.js
@@ -0,0 +1,11 @@
+//script.js
+const course = [
+    {name:'数学',score:90},
+    {name:'英语',score:85},
+    {name:'物理',score:78},
+    {name:'化学',score:69},
+    {name:'生物',score:88},
+    {name:'历史',score:105},
+    {name:'政治',score:-3},
+];
+
